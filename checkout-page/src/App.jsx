7c6e2a5503b9a86cfe9e914172092c655266edcc
Praveen Kumar <22a91a05b0@aec.edu.in>
2026-02-27 17:06:40 +0530

    Merge async implementation from oldrepo

--- checkout-page/src/App.jsx
+++ checkout-page/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Checkout from './pages/Checkout';
 
+
+
 export default function App() {
   return (
     <BrowserRouter>
